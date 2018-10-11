84496eec347e3b16791ed637c13c8f4c4544bb1b
Josh <josh@creativeworks.com.au>
2018-10-11 12:00:17 +0800

    Changes

--- targetLine.h
+++ targetLine.h
@@ -11,24 +11,28 @@ void targetLine(char* line);
 void targetLine(char* line) {
     char *target = firstWord(line, ':');
     char *dependencies = endingOfLine(line, ':');
-    char* dependencyList[20]; // Used to store all the dependencies 
-    int numDependencies = 0;
-    char* dependency;
-    char buf[50]; // Used for the max number of characters of a dependency
+    if (strcmp(dependencies, "") != 0) {
+        char* dependencyList[20]; // Used to store all the dependencies 
+        int numDependencies = 0;
+        char* dependency;
+        char buf[50]; // Used for the max number of characters of a dependency
 
-    strcpy(buf,dependencies);
-    dependency = buf;
-    for (int i = 0; buf[i] != '\0'; i++) {
-        if (buf[i] == ' ') {
-            buf[i] = '\0';
-            dependencyList[numDependencies] = dependency;
-            numDependencies++;
-            dependency = buf + i + 1;
+        strcpy(buf,dependencies);
+        dependency = buf;
+        for (int i = 0; buf[i] != '\0'; i++) {
+            if (buf[i] == ' ') {
+                buf[i] = '\0';
+                dependencyList[numDependencies] = dependency;
+                numDependencies++;
+                dependency = buf+i+1;
+            }
         }
-    }
-    dependencyList[numDependencies] = dependency;
-    for (int i=0; i < numDependencies+1; i++) {
-        // Check date last modified
-        printf("%s\n", dependencyList[i]);
+        if (dependency != NULL) {
+            dependencyList[numDependencies] = dependency;
+        }
+        for (int i=0; i < numDependencies+1; i++) {
+            // Check date last modified
+            printf("%s\n", dependencyList[i]);
+        }
     }
 }
